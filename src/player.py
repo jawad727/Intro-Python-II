44a5d57e5171832dae59f7acbb59c68441ed15d7
jawad727 <jawaddhussein@gmail.com>
2019-06-05 14:28:53 -0700

    classes created

--- src/player.py
+++ src/player.py
@@ -1,2 +1,13 @@
 # Write a class to hold player information, e.g. what room they are in
 # currently.
+
+class Player:
+    def __init__(self, name, currentroom):
+        self.name = name
+        self.currentroom = currentroom
+
+    def __str__(self):
+        return f'{self.name}, {self.currentroom}'
+
+play = Player('name', 'currentroom')
+print(play)
